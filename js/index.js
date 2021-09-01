262edabe8360da6c6ae51d95e06bf39b599bb2fc
Yanina Andregnette <62681163+YannEvilTwin@users.noreply.github.com>
2021-08-31 21:35:07 -0300

    Actualizacion del contenido

--- js/index.js
+++ js/index.js
@@ -1,47 +1,3 @@
-// Colores random
-const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
-
-const numBalls = 50;
-const balls = [];
-
-for (let i = 0; i < numBalls; i++) {
-  let ball = document.createElement("div");
-  ball.classList.add("ball");
-  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-  ball.style.transform = `scale(${Math.random()})`;
-  ball.style.width = `${Math.random()}em`;
-  ball.style.height = ball.style.width;
-
-  balls.push(ball);
-  document.body.append(ball);
-}
-
-// Keyframes
-balls.forEach((el, i, ra) => {
-  let to = {
-    x: Math.random() * (i % 2 === 0 ? -11 : 11),
-    y: Math.random() * 12
-  };
-
-  let anim = el.animate(
-    [{
-        transform: "translate(0, 0)"
-      },
-      {
-        transform: `translate(${to.x}rem, ${to.y}rem)`
-      }
-    ], {
-      duration: (Math.random() + 1) * 2000, // random duration
-      direction: "alternate",
-      fill: "both",
-      iterations: Infinity,
-      easing: "ease-in-out"
-    }
-  );
-});
-
 //Navbar
 
 function nav(evt, pageName) {
@@ -85,3 +41,77 @@ window.onclick = function (event) {
 };
 
 //Fin Navbar
+
+//Video slider
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {  
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i,
+      x = document.getElementsByClassName("video-slide"),
+      y = document.getElementsByTagName("video");
+  
+  if (n > x.length) {
+    slideIndex = 1
+  }
+  
+  if (n < 1) {
+    slideIndex = x.length
+  }
+  
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+    y[i].pause();
+  }
+  
+  x[slideIndex-1].style.display = "block";
+  
+}
+
+// Colores random
+const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
+
+const numBalls = 30;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [{
+        transform: "translate(0, 0)"
+      },
+      {
+        transform: `translate(${to.x}rem, ${to.y}rem)`
+      }
+    ], {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+});
